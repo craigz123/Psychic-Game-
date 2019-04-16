5f8de396f479c5cc9f09338a11a5a4799419cb10
craigz123 <czimbler60@gmail.com>
2019-04-16 18:18:57 -0400

    working app

---
+++ assets/javascript/game.js
@@ -0,0 +1,59 @@
+//Letters array
+const lettersArr = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+
+//Game variables
+let playerGuesses = [];
+let guessesLeft = 9;
+let wins = 0;
+let losses = 0;
+let randomLetter = '';
+let userInput = "";
+
+//Add the beginning values to the browser
+document.querySelector("#wins").innerHTML = wins;
+document.querySelector("#losses").innerHTML = losses;
+document.querySelector("#guesses").innerHTML = guessesLeft;
+
+//Game chooses a random letter
+randComp();
+
+document.addEventListener('keydown', function (event) {
+    guessesLeft = guessesLeft - 1;
+    userInput = event.key;
+
+    if (userInput === randomLetter) {
+        wins = wins + 1
+        document.querySelector("#wins").innerHTML = wins;
+        resetGame();
+    } else if (guessesLeft === 0) {
+        losses = losses + 1
+        document.querySelector("#losses").innerHTML = losses;
+        playerGuesses = [];
+        document.querySelector('#letters-guessed').innerHTML = playerGuesses;
+        guessesLeft = 9;
+        document.querySelector("#guesses").innerHTML = guessesLeft;
+        randComp();
+    } else if (userInput !== randomLetter && guessesLeft > 0) {
+        document.querySelector("#guesses").innerHTML = guessesLeft;
+        playerGuesses.push(userInput);
+        document.querySelector('#letters-guessed').innerHTML = playerGuesses;
+    }
+    console.log('user input', userInput);
+});
+
+
+//FUNCTIONS
+const randComp = () => {
+    let randLet = Math.floor(Math.random() * 25);
+    randomLetter = lettersArr[randLet];
+    console.log('random letter', randomLetter);
+};
+
+const resetGame = () => {
+    randComp();
+    guessesLeft = 9;
+    playerGuesses = [];
+
+    document.querySelector("#guesses").innerHTML = guessesLeft;
+    document.querySelector('#letters-guessed').innerHTML = playerGuesses;
+};
